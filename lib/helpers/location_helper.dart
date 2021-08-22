50da69b18211c40f0aa5b4bd933d76dfc675ef29
Beren Erchamion <beren@houseofbeor.net>
2021-08-22 10:56:10 -0400

    Adding functions to keepAPI keys out of git

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,10 +1,12 @@
-const GOOGLE_API_KEY = '';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class LocationHelper {
+
   static String generatePixelPlacePreview({
     required double latitude,
     required double longitude,
   }) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude&$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$GOOGLE_API_KEY';
+    final String _googleApiKey = dotenv.get('GOOGLE_API_KEY');
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude&$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7Clabel:C%7C$latitude,$longitude&key=$_googleApiKey';
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import './screens/places_list_screen.dart';
 import './screens/add_place_screen.dart';
 
 import './providers/pixel_places_provider.dart';
 
-void main() {
+Future main() async{
+  await dotenv.load(fileName: '.env');
   runApp(MyApp());
 }
 
